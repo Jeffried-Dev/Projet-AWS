a569e7a0f3101564bdef1b121368b502363d340c
uvsq22308873 <146740166+uvsq22308873@users.noreply.github.com>
2024-02-28 14:09:36 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    container: {
+      maxWidth: {
+        default: '800px'
+      }
+    },
+  },
+  plugins: [],
+}
+
